e7d729870a5716a5ad6f88409437dd44fa8bc751
Joe McKinney <93625431+Joecode22@users.noreply.github.com>
2023-12-05 13:03:43 -0600

    Create ufo.cpp

---
+++ ufo.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "ufo_functions.hpp"
+
+int main() {
+  greet();
+  std::string codeword = "codecademy";
+  std::string answer = "__________";
+  int misses = 0;
+  bool guess = false;
+  std::vector<char> incorrect;
+
+  //get player input
+  char letter;
+
+
+  while (answer != codeword && misses < 7) {
+    guess = false;
+    display_misses(misses);
+    display_status(incorrect, answer);
+    std::cout << "\nPlease enter your guess: \n";
+    std::cin >> letter;
+    for (int i = 0; i < codeword.size(); i++) {
+      if(codeword[i] == letter) {
+        answer[i] = letter;
+        guess = true;
+      }
+    }
+    if(guess) {
+      std::cout << "\nCorrect! You're closer to cracking the codeword.\n";
+    } else {
+      std::cout << "\nIncorrect! The tractor beam pulls the person in further.\n";
+      incorrect.push_back(letter);
+      misses++;
+    }
+  }
+  end_game(answer, codeword);
+}
